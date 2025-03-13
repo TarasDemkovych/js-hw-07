ecddf82acde8ae81921cb83edfb777b5ee884b95
1010 <uafenomeno@gmail.com>
2025-03-13 11:22:09 +0100

    js-hw-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,10 @@
-class StringBuilder {
-  #value;
-  constructor(value) {
-    this.#value = value;
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+input.addEventListener('input', function () {
+  let name = input.value.trim();
+  if (name === '') {
+    output.textContent = 'Anonymous';
+  } else {
+    output.textContent = name;
   }
-  getValue() {
-    return this.#value;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padEnd(str) {
-    this.#value = this.#value + str;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+});
